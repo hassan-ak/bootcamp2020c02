11c9b935040c59b6b1433343dc0a5671dce3e180
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-25 10:26:26 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hassan Ali Khan
         </p>
         <a
           className="App-link"
